4994ca07ff03f1cbffc47c50dc5f48186723635c
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-11-11 12:39:58 -0500

    ben-add: shared UI lib

---
+++ apps/ngrx-playground/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { SharedUiSharedUiComponent } from '@angular-monorepo/shared/ui/shared-ui';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, SharedUiSharedUiComponent],
+  selector: 'angular-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'ngrx-playground';
+}
---
+++ libs/shared/ui/shared-ui/src/lib/shared-ui-shared-ui/shared-ui-shared-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-monorepo-shared-ui-shared-ui',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shared-ui-shared-ui.component.html',
+  styleUrls: ['./shared-ui-shared-ui.component.css'],
+})
+export class SharedUiSharedUiComponent {}
